323e685d4db5e6045953109339a4c23de13c56fd
Aaren <aaren.yang@gmail.com>
2024-05-12 14:45:31 +0800

    Fix

--- Arma_ScreenShot_Extension/DllEntry.cs
+++ Arma_ScreenShot_Extension/DllEntry.cs
@@ -1,32 +1,34 @@
-﻿using Maca134.Arma.DllExport;
+﻿using RGiesecke.DllExport;
+using Maca134.Arma.DllExport;
 using System.Text;
-using RGiesecke.DllExport;
+using System.Runtime.InteropServices;
 
 namespace Arma_ScreenShot_Extension
 {
-  public class DllEntry
-  {
-    [ArmaDllExport]
+    public class DllEntry
+    {
+        [ArmaDllExport]
 
-    #region Misc RVExtension Requirements
+        #region Misc RVExtension Requirements
 #if IS_x64
     [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
 #else
-    [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
+        [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
 #endif
-    public static void RvExtensionVersion(StringBuilder output, int outputSize)
-    {
-      outputSize--;
-      output.Append("1.0.0");
-    }
+        public static void RvExtensionVersion(StringBuilder output, int outputSize)
+        {
+            outputSize--;
+            output.Append("1.0.0");
+        }
 
-    #endregion
+        #endregion
 
-    public static int RvExtensionArgs(StringBuilder output, int outputSize,
-      [MarshalAs(UnmanagedType.LPStr)] string inputKey,
-      [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount)
-    {
-      output.Append("ScreenShot Test");
-      return 1;
+        public static int RvExtensionArgs(StringBuilder output, int outputSize,
+          [MarshalAs(UnmanagedType.LPStr)] string inputKey,
+          [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount)
+        {
+            output.Append("ScreenShot Test");
+            return 1;
+        }
     }
-  }
+}
