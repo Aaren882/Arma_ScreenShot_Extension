fed4ee67c37c7a3daa08693c9120d197a9a736f9
Aaren <aaren.yang@gmail.com>
2024-05-12 14:36:12 +0800

    TakeScreenShot Function

---
+++ Arma_ScreenShot_Extension/TakeScreenShot.cs
@@ -0,0 +1,11 @@
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Arma_ScreenShot_Extension
+{
+  static class TakeScreenShot
+  {
+    private 
+  }
+}
