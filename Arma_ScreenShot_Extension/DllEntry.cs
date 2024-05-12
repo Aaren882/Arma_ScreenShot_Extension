0513a9335e5b46132f55f4c33c8e7bf0b48cf95a
Aaren <aaren.yang@gmail.com>
2024-05-12 15:12:49 +0800

    Test

--- Arma_ScreenShot_Extension/DllEntry.cs
+++ Arma_ScreenShot_Extension/DllEntry.cs
@@ -7,28 +7,45 @@ namespace Arma_ScreenShot_Extension
 {
     public class DllEntry
     {
-        [ArmaDllExport]
+      [ArmaDllExport]
 
-        #region Misc RVExtension Requirements
+    #region Misc RVExtension Requirements
 #if IS_x64
-        [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
+    [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
 #else
-        [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
+    [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
 #endif
-        public static void RvExtensionVersion(StringBuilder output, int outputSize)
-        {
-            outputSize--;
-            output.Append("1.0.0");
-        }
+    public static void RvExtensionVersion(StringBuilder output, int outputSize)
+    {
+      outputSize--;
+      output.Append("1.0.0");
+    }
 
-        #endregion
+#if IS_x64
+    [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
+#else
+    [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
+#endif
+    public static void RvExtension(StringBuilder output, int outputSize,
+    [MarshalAs(UnmanagedType.LPStr)] string function)
+    {
+      outputSize--;
+      output.Append("ScreenShot TEST");
+    }
 
-        public static int RvExtensionArgs(StringBuilder output, int outputSize,
+#if IS_x64
+    [DllExport("RVExtensionArgs", CallingConvention = CallingConvention.Winapi)]
+#else
+    [DllExport("_RVExtensionArgs@20", CallingConvention = CallingConvention.Winapi)]
+#endif
+    #endregion
+
+    /*public static int RvExtensionArgs(StringBuilder output, int outputSize,
           [MarshalAs(UnmanagedType.LPStr)] string inputKey,
           [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount)
         {
             output.Append("ScreenShot Test");
             return 1;
-        }
+        }*/
     }
 }
