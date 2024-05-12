68c78cc67528c818d72affc492f612a18aca2f71
Aaren <aaren.yang@gmail.com>
2024-05-12 16:03:33 +0800

    remove ArmaDLL Export

--- Arma_ScreenShot_Extension/DllEntry.cs
+++ Arma_ScreenShot_Extension/DllEntry.cs
@@ -1,5 +1,4 @@
 ﻿using RGiesecke.DllExport;
-using Maca134.Arma.DllExport;
 using System.Text;
 using System.Runtime.InteropServices;
 
@@ -7,57 +6,57 @@ namespace Arma_ScreenShot_Extension
 {
   public class DllEntry
   {
-    #region Misc RVExtension Requirements
+        #region Misc RVExtension Requirements
 
-      #if IS_x64
-        [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
-      #else
-        [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
-      #endif
+          #if IS_x64
+            [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
+          #else
+            [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
+          #endif
 
-      public static void RvExtensionVersion(StringBuilder output, int outputSize)
-      {
-        outputSize--;
-        output.Append("1.0.0");
-      }
+          public static void RvExtensionVersion(StringBuilder output, int outputSize)
+          {
+            outputSize--;
+            output.Append("1.0.0");
+          }
 
-      #if IS_x64
-        [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
-      #else
-        [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
-      #endif
+          #if IS_x64
+            [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
+          #else
+            [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
+          #endif
 
-      public static void RvExtension(StringBuilder output, int outputSize,
-          [MarshalAs(UnmanagedType.LPStr)] string function)
+          public static void RvExtension(StringBuilder output, int outputSize,
+              [MarshalAs(UnmanagedType.LPStr)] string function)
+            {
+              outputSize--;
+              if (function == "init")
+              {
+                output.Append("TEST TEST Init");
+              }
+            }
+
+          #if IS_x64
+            [DllExport("RVExtensionArgs", CallingConvention = CallingConvention.Winapi)]
+          #else
+            [DllExport("_RVExtensionArgs@20", CallingConvention = CallingConvention.Winapi)]
+          #endif
+        #endregion
+
+        public static int RvExtensionArgs(StringBuilder output, int outputSize,
+          [MarshalAs(UnmanagedType.LPStr)] string inputKey,
+          [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount)
         {
           outputSize--;
-          if (function == "init")
+          try
           {
-            output.Append("TEST TEST Init");
+            output.Append("TEST TEST Try");
           }
+          catch (Exception e)
+          {
+            output.Append("TEST TEST Catch");
+          }
+          return 1;
         }
-
-      #if IS_x64
-        [DllExport("RVExtensionArgs", CallingConvention = CallingConvention.Winapi)]
-      #else
-        [DllExport("_RVExtensionArgs@20", CallingConvention = CallingConvention.Winapi)]
-      #endif
-    #endregion
-
-    public static int RvExtensionArgs(StringBuilder output, int outputSize,
-      [MarshalAs(UnmanagedType.LPStr)] string inputKey,
-      [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount)
-    {
-      outputSize--;
-      try
-      {
-        output.Append("TEST TEST Try");
-      }
-      catch (Exception e)
-      {
-        output.Append("TEST TEST Catch");
-      }
-      return 1;
     }
-  }
 }
