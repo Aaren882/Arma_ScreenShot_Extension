d1dbc0266f7412c2e6f50eef5acc91beb7d339be
Aaren <aaren.yang@gmail.com>
2024-05-12 16:17:13 +0800

    tets

--- Arma_ScreenShot_Extension/DllEntry.cs
+++ Arma_ScreenShot_Extension/DllEntry.cs
@@ -6,44 +6,52 @@ namespace Arma_ScreenShot_Extension
 {
   public class DllEntry
   {
-        #region Misc RVExtension Requirements
+    private static readonly string SessionKey = "WWW";
+    private static bool InitComplete = false;
 
-          #if IS_x64
-            [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
-          #else
-            [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
-          #endif
+    #region Misc RVExtension Requirements
+#if IS_x64
+        [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
+#else
+    [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
+#endif
+    public static void RvExtensionVersion(StringBuilder output, int outputSize)
+    {
+      outputSize--;
+      output.Append("1.0.0");
+    }
 
-          public static void RvExtensionVersion(StringBuilder output, int outputSize)
-          {
-            outputSize--;
-            output.Append("1.0.0");
-          }
+#if IS_x64
+        [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
+#else
+    [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
+#endif
+    public static void RvExtension(StringBuilder output, int outputSize,
+        [MarshalAs(UnmanagedType.LPStr)] string function)
+    {
+      outputSize--;
+      if (function == "init")
+      {
+        if (!InitComplete)
+        {
+          InitComplete = true;
+          //Tools.Logger(null, "Initialized");
 
-          #if IS_x64
-            [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
-          #else
-            [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
-          #endif
+          output.Append(SessionKey);
+        }
+        else
+          Tools.Logger(null, "Attempted re-initialization");
+      }
+    }
 
-          public static void RvExtension(StringBuilder output, int outputSize,
-              [MarshalAs(UnmanagedType.LPStr)] string function)
-            {
-              outputSize--;
-              if (function == "init")
-              {
-                output.Append("TEST TEST Init");
-              }
-            }
+#if IS_x64
+        [DllExport("RVExtensionArgs", CallingConvention = CallingConvention.Winapi)]
+#else
+    [DllExport("_RVExtensionArgs@20", CallingConvention = CallingConvention.Winapi)]
+#endif
+    #endregion
 
-          #if IS_x64
-            [DllExport("RVExtensionArgs", CallingConvention = CallingConvention.Winapi)]
-          #else
-            [DllExport("_RVExtensionArgs@20", CallingConvention = CallingConvention.Winapi)]
-          #endif
-        #endregion
-
-        public static int RvExtensionArgs(StringBuilder output, int outputSize,
+    public static int RvExtensionArgs(StringBuilder output, int outputSize,
           [MarshalAs(UnmanagedType.LPStr)] string inputKey,
           [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount)
         {
