5a07854ad656790dbab5af0ffeea31759d6778c1
Aaren <aaren.yang@gmail.com>
2024-05-12 14:03:17 +0800

    加入專案檔案。

---
+++ Arma_ScreenShot_Extension/DllEntry.cs
@@ -0,0 +1,14 @@
+﻿using Maca134.Arma.DllExport;
+using System.Text;
+
+namespace Arma_ScreenShot_Extension
+{
+    public class DllEntry
+    {
+        [ArmaDllExport]
+       static void RvExtension(StringBuilder output, int outputSize)
+       {
+            return;
+       }
+    }
+}
