f4cc8cdd529ce46c19cb1bfe5874c214f0141569
Aaren <aaren.yang@gmail.com>
2024-05-12 14:39:50 +0800

    Test

--- Arma_ScreenShot_Extension/DllEntry.cs
+++ Arma_ScreenShot_Extension/DllEntry.cs
@@ -4,12 +4,29 @@ using RGiesecke.DllExport;
 
 namespace Arma_ScreenShot_Extension
 {
-    public class DllEntry
+  public class DllEntry
+  {
+    [ArmaDllExport]
+
+    #region Misc RVExtension Requirements
+#if IS_x64
+    [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
+#else
+    [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
+#endif
+    public static void RvExtensionVersion(StringBuilder output, int outputSize)
     {
-        [ArmaDllExport]
-       static void RvExtension(StringBuilder output, int outputSize)
-       {
-            output.Append("TEst DLL Entry");
-       }
+      outputSize--;
+      output.Append("1.0.0");
     }
-}
+
+    #endregion
+
+    public static int RvExtensionArgs(StringBuilder output, int outputSize,
+      [MarshalAs(UnmanagedType.LPStr)] string inputKey,
+      [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount)
+    {
+      output.Append("ScreenShot Test");
+      return 1;
+    }
+  }
