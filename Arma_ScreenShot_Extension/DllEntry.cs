71fd1a3ec28575d1fcc71d97a94c185e854945cd
Aaren <aaren.yang@gmail.com>
2024-05-12 17:13:19 +0800

    test

--- Arma_ScreenShot_Extension/DllEntry.cs
+++ Arma_ScreenShot_Extension/DllEntry.cs
@@ -8,7 +8,7 @@ namespace Arma_ScreenShot_Extension
   {
     #region Misc RVExtension Requirements
     #if IS_x64
-            [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
+        [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
     #else
         [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
     #endif
@@ -19,26 +19,16 @@ namespace Arma_ScreenShot_Extension
       }
 
     #if IS_x64
-            [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
+        [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
     #else
-      [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
+        [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
     #endif
     public static void RvExtension(StringBuilder output, int outputSize,
         [MarshalAs(UnmanagedType.LPStr)] string function)
     {
-      outputSize--;
-      if (function == "init")
-      {
-        if (!InitComplete)
-        {
-          InitComplete = true;
-          //Tools.Logger(null, "Initialized");
-
-          output.Append(SessionKey);
-        }
-        else
-          Tools.Logger(null, "Attempted re-initialization");
-      }
+        outputSize--;
+        Tools.Logger(null, function);
+        output.Append(function);
     }
     #endregion
   }
--- Arma_ScreenShot_Extension/Tools.cs
+++ Arma_ScreenShot_Extension/Tools.cs
@@ -1,10 +1,6 @@
-using System;
-using System.IO;
-using System.Net.Http;
 using System.Reflection;
-using System.Threading.Tasks;
 
-namespace DiscordMessageAPI
+namespace Arma_ScreenShot_Extension
 {
     internal class Tools
     {
