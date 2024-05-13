56bf3219c698e79a8987da2df404ca9afc542424
Aaren <aaren.yang@gmail.com>
2024-05-13 18:01:52 +0800

    Finish

--- Arma_ScreenShot_Extension/DllEntry.cs
+++ Arma_ScreenShot_Extension/DllEntry.cs
@@ -1,64 +1,50 @@
 ﻿using RGiesecke.DllExport;
 using System;
+using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Arma_ScreenShot_Extension
 {
-  public class DllEntry
-  {
+    public class DllEntry
+    {
 
-    #region Misc RVExtension Requirements
+        #region Misc RVExtension Requirements
 #if IS_x64
-    [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
+        [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
 #else
     [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
 #endif
-    public static void RvExtensionVersion(StringBuilder output, int outputSize)
-    {
-      outputSize--;
-      output.Append("1.0.0");
-    }
+        public static void RvExtensionVersion(StringBuilder output, int outputSize)
+        {
+            outputSize--;
+            output.Append("1.0.0");
+        }
 
 #if IS_x64
-    [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
+        [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
 #else
     [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
 #endif
-    public static void RvExtension(StringBuilder output, int outputSize,
-        [MarshalAs(UnmanagedType.LPStr)] string function)
-    {
-      outputSize--;
-      if (function == "init")
-      {
-        output.Append("Init");
-      }
-    }
+        public static void RvExtension(StringBuilder output, int outputSize,
+            [MarshalAs(UnmanagedType.LPStr)] string path)
+        {
+            outputSize--;
+            output.Append(new ScreenCapture().TakeScreenshot(path));
+        }
 
 #if IS_x64
-    [DllExport("RVExtensionArgs", CallingConvention = CallingConvention.Winapi)]
+        [DllExport("RVExtensionArgs", CallingConvention = CallingConvention.Winapi)]
 #else
     [DllExport("_RVExtensionArgs@20", CallingConvention = CallingConvention.Winapi)]
 #endif
-    #endregion
-    public static int RvExtensionArgs(StringBuilder output, int outputSize,
-        [MarshalAs(UnmanagedType.LPStr)] string inputKey,
-        [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount)
-    {
-      outputSize--;
-      try
-      {
-        if (args.Length == 2)
+        #endregion
+        public static int RvExtensionArgs(StringBuilder output, int outputSize,
+            [MarshalAs(UnmanagedType.LPStr)] string inputKey,
+            [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount)
         {
-          ScreenCapture.TakeScreenshot(args);
+            return 1;
         }
-        output.Append("INCORRECT NUMBER OF ARGUMENTS");
-      }
-      catch (Exception e)
-      {
-        Tools.Logger(e);
-      };
-      return 1;
     }
-  }
 }
--- Arma_ScreenShot_Extension/ScreenCapture.cs
+++ Arma_ScreenShot_Extension/ScreenCapture.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace Arma_ScreenShot_Extension
 {
@@ -21,28 +22,32 @@ namespace Arma_ScreenShot_Extension
             public int Bottom;
         }
 
-        /*static void Main(string[] args)
+        public string TakeScreenshot(string outputFilePath)
         {
-            TakeScreenshot(@"K:\Screenshot\snippetsource.jpg");
-            Console.WriteLine("Screenshot taken!");
-        }*/
-
-        static void TakeScreenshot(string outputFilePath)
-        {
-            IntPtr handle = GetForegroundWindow();
-            RECT rect;
-            GetWindowRect(handle, out rect);
-
-            int width = rect.Right - rect.Left;
-            int height = rect.Bottom - rect.Top;
-
-            using (Bitmap bitmap = new Bitmap(width, height))
+            try
             {
-                using (Graphics g = Graphics.FromImage(bitmap))
+                IntPtr handle = GetForegroundWindow();
+                RECT rect;
+                GetWindowRect(handle, out rect);
+
+                int width = rect.Right - rect.Left;
+                int height = rect.Bottom - rect.Top;
+                //outputFilePath = outputFilePath.Trim('"',' ').Replace("/", @"\");
+
+                using (Bitmap bitmap = new Bitmap(width, height))
                 {
-                    g.CopyFromScreen(rect.Left, rect.Top, 0, 0, new Size(width, height));
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+                        g.CopyFromScreen(rect.Left, rect.Top, 0, 0, new Size(width, height));
+                    }
+
+                    bitmap.Save(outputFilePath);
                 }
-                bitmap.Save(outputFilePath);
+                return outputFilePath;
+            }
+            catch (Exception i)
+            {
+                return $"Error: \n{i}\n{outputFilePath}";
             }
         }
     }
